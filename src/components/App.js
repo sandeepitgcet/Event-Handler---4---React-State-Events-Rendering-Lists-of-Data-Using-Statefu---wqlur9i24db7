134763f0cc474b59f2471d0c6639f16cafe03f3a
Sandeep Singh <33353409+sandeepitgcet@users.noreply.github.com>
2023-01-07 19:03:56 +0000

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
-
+  const [isClicked, setIsClicked] = useState(false)
   const handleDoubleClick = (event) =>{
-  
+      setIsClicked(!isClicked);
+      if(isClicked){
+        console.log("I was double clicked");
+      }else{
+        console.log("I was not double clicked")
+      }
   }
   return (
     <div id="main">
